f2e1816e459768f63fac3ddd26b379adb68f045b
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-05-24 19:10:34 -0300

    Atividade

---
+++ src/main/java/com/tcdev/desafio_modelo_dominio_orm/entities/Atividade.java
@@ -0,0 +1,68 @@
+package com.tcdev.desafio_modelo_dominio_orm.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_name")
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    private Atividade atividade;
+
+    public Atividade() {}
+
+    public Atividade(Long id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Atividade{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
