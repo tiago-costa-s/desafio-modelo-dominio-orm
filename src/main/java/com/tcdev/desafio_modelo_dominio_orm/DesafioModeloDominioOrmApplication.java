3e0f5438f75f1a94d88b5c9ad5e8be75eb4d4063
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-05-24 18:54:52 -0300

    first commit

---
+++ src/main/java/com/tcdev/desafio_modelo_dominio_orm/DesafioModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.tcdev.desafio_modelo_dominio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcdev/desafio_modelo_dominio_orm/DesafioModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcdev.desafio_modelo_dominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
