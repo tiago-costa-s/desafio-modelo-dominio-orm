af5c11637ecbc594287e3b6464e857b83453903d
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-05-24 19:01:17 -0300

    Categoria

--- src/main/java/com/tcdev/desafio_modelo_dominio_orm/entities/Categoria.java
+++ src/main/java/com/tcdev/desafio_modelo_dominio_orm/entities/Categoria.java
@@ -1,9 +1,6 @@
 package com.tcdev.desafio_modelo_dominio_orm.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_categoria")
